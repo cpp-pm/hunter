2ba8f72623db01885930173bd65e45aaf6e58279
Joerg-Christian Boehme <joerg@chaosdorf.de>
2019-12-12 08:29:53 +0100

    Remove commented out lines

--- cmake/projects/tiny-process-library/hunter.cmake
+++ cmake/projects/tiny-process-library/hunter.cmake
@@ -2,8 +2,6 @@
 
 include(hunter_add_version)
 include(hunter_cacheable)
-#include(hunter_cmake_args)
-#include(hunter_configuration_types)
 include(hunter_download)
 include(hunter_pick_scheme)
 
@@ -18,25 +16,9 @@ hunter_add_version(
     380589052c93ca6815717d0fd62270c6dc867eef
 )
 
-#if (ANDROID OR IOS)
-# hunter_cmake_args(
-#    tiny-process-library
-#    CMAKE_ARGS
-#    PKGCONFIG_EXPORT_TARGETS=libfoo;libbar
-#    DEPENDS_ON_PACKAGES=foo;bar
-#    EXTRA_FLAGS=--enable-bots
-# )
-#endif()
-
-#hunter_configuration_types(tiny-process-library CONFIGURATION_TYPES Release)
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(tiny-process-library)
 hunter_download(
     PACKAGE_NAME tiny-process-library
     PACKAGE_INTERNAL_DEPS_ID "1"
-#     PACKAGE_UNRELOCATABLE_TEXT_FILES
-#     "lib/libfoo.la"
-#     "lib/libpbar.la"
-#     "lib/pkgconfig/libfoo.pc"
-#     "lib/pkgconfig/libpbar.pc"
 )
