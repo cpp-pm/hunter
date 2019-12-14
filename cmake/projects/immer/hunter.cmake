d6387937d14873df80dc80e44d4a2d69f2d03311
Joerg-Christian Boehme <joerg@chaosdorf.de>
2019-12-14 13:46:57 +0100

    Initial commit

---
+++ cmake/projects/immer/hunter.cmake
@@ -0,0 +1,42 @@
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+#include(hunter_cmake_args)
+#include(hunter_configuration_types)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    immer
+    VERSION
+    1.0.0
+    URL
+    "https://github.com/hunter-packages/foo/archive/v1.0.0.tar.gz"
+    SHA1
+    1111111111111111111111111111111111111111
+)
+
+#if (ANDROID OR IOS)
+# hunter_cmake_args(
+#    immer
+#    CMAKE_ARGS
+#    PKGCONFIG_EXPORT_TARGETS=libfoo;libbar
+#    DEPENDS_ON_PACKAGES=foo;bar
+#    EXTRA_FLAGS=--enable-bots
+# )
+#endif()
+
+#hunter_configuration_types(immer CONFIGURATION_TYPES Release)
+hunter_pick_scheme(DEFAULT url_sha1_autotools)
+hunter_cacheable(immer)
+hunter_download(
+    PACKAGE_NAME immer
+    PACKAGE_INTERNAL_DEPS_ID "1"
+#     PACKAGE_UNRELOCATABLE_TEXT_FILES
+#     "lib/libfoo.la"
+#     "lib/libpbar.la"
+#     "lib/pkgconfig/libfoo.pc"
+#     "lib/pkgconfig/libpbar.pc"
+)
---
+++ docs/packages/pkg/immer.rst
@@ -0,0 +1,21 @@
+.. spelling::
+
+    immer
+
+.. index::
+  single: unsorted ; immer
+
+.. _pkg.immer:
+
+immer
+===
+
+-  `Official <https://official_foo>`__
+-  `Hunterized <https://github.com/hunter-packages/foo>`__
+-  `Example <https://github.com/ruslo/hunter/blob/master/examples/immer/CMakeLists.txt>`__
+-  Added by `foo_developer <https://github.com/foo_developer>`__ (`pr-N <https://github.com/ruslo/hunter/pull/N>`__)
+
+.. literalinclude:: /../examples/immer/CMakeLists.txt
+  :language: cmake
+  :start-after: # DOCUMENTATION_START {
+  :end-before: # DOCUMENTATION_END }
