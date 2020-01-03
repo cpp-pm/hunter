dd7596be26936f68c556e3d5fae2d4282de916b3
drodin <rodin.dmitry@gmail.com>
2020-01-03 12:41:12 +0700

    Add 'FLAC' package

---
+++ cmake/projects/FLAC/hunter.cmake
@@ -0,0 +1,39 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    FLAC
+    VERSION
+    1.3.3-p0
+    URL
+    "https://github.com/drodin/flac/archive/1.3.3-p0.tar.gz"
+    SHA1
+    a573e68951ee74568ba7c05c5e0f874313975932
+)
+
+set(
+    _hunter_flac_cmake_args
+    WITH_OGG=OFF
+)
+
+if(ANDROID OR IOS)
+  list(
+      APPEND
+      _hunter_flac_cmake_args
+      BUILD_EXECUTABLES=OFF
+      BUILD_EXAMPLES=OFF
+      BUILD_TESTING=OFF
+      BUILD_DOXYGEN=OFF
+      WITH_ASM=OFF
+  )
+endif()
+
+hunter_cmake_args(FLAC CMAKE_ARGS ${_hunter_flac_cmake_args})
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(FLAC)
+hunter_download(PACKAGE_NAME FLAC)
