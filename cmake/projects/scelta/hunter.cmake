c6ccdd4086b2e047dd3c11cefde5adb8744a754a
Joerg-Christian Boehme <joerg@chaosdorf.de>
2020-01-27 11:44:39 +0100

    Add new scelta version and fix findings

--- cmake/projects/scelta/hunter.cmake
+++ cmake/projects/scelta/hunter.cmake
@@ -2,8 +2,6 @@
 
 include(hunter_add_version)
 include(hunter_cacheable)
-#include(hunter_cmake_args)
-#include(hunter_configuration_types)
 include(hunter_download)
 include(hunter_pick_scheme)
 
@@ -11,32 +9,16 @@ hunter_add_version(
     PACKAGE_NAME
     scelta
     VERSION
-    0.1.0-a0f4f70
+    0.1.0-a0f4f70-p0
     URL
-    "https://github.com/Bjoe/scelta/archive/594aed957f4c2867243c08b73af49ef4d4599520.tar.gz"
+    "https://github.com/cpp-pm/scelta/archive/v0.1-a0f4f70-p0.tar.gz"
     SHA1
-    5a0fc037df80027af09de094875af8cac9b8d19f
+    2b5abc5e577687757f4cc37d702e5903fb143a9a
 )
 
-#if (ANDROID OR IOS)
-# hunter_cmake_args(
-#    scelta
-#    CMAKE_ARGS
-#    PKGCONFIG_EXPORT_TARGETS=libfoo;libbar
-#    DEPENDS_ON_PACKAGES=foo;bar
-#    EXTRA_FLAGS=--enable-bots
-# )
-#endif()
-
-#hunter_configuration_types(scelta CONFIGURATION_TYPES Release)
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(scelta)
 hunter_download(
     PACKAGE_NAME scelta
     PACKAGE_INTERNAL_DEPS_ID "1"
-#     PACKAGE_UNRELOCATABLE_TEXT_FILES
-#     "lib/libfoo.la"
-#     "lib/libpbar.la"
-#     "lib/pkgconfig/libfoo.pc"
-#     "lib/pkgconfig/libpbar.pc"
 )
--- docs/packages/pkg/scelta.rst
+++ docs/packages/pkg/scelta.rst
@@ -13,7 +13,7 @@ scelta
 -  `Official <https://github.com/SuperV1234/scelta>`__
 -  `Hunterized <https://github.com/cpp-pm/scelta>`__
 -  `Example <https://github.com/cpp-pm/hunter/blob/master/examples/scelta/CMakeLists.txt>`__
--  Added by `Joerg-Christian Boehme <https://github.com/Bjoe>`__ (`pr-N <https://github.com/cpp-pm/hunter/pull/N>`__)
+-  Added by `Joerg-Christian Boehme <https://github.com/Bjoe>`__ (`pr-142 <https://github.com/cpp-pm/hunter/pull/142>`__)
 
 .. literalinclude:: /../examples/scelta/CMakeLists.txt
   :language: cmake
