e686217a4cf9895c873f3c2d3b6904d718a40f90
Harry Mallon <hjmallon@gmail.com>
2020-03-06 10:37:42 +0000

    Update 'IlmBase' to 2.4.0-p1

--- cmake/projects/IlmBase/hunter.cmake
+++ cmake/projects/IlmBase/hunter.cmake
@@ -21,6 +21,17 @@ hunter_add_version(
     a99fb3c35b9d9c2e4aeb2cd38e7cb1149619ca0e
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    IlmBase
+    VERSION
+    2.4.0-p1
+    URL
+    "https://github.com/cpp-pm/openexr/archive/v2.4.0-p1.tar.gz"
+    SHA1
+    844f501f28b59caa3bcdf093dacae7a067429694
+)
+
 hunter_cmake_args(
     IlmBase
     CMAKE_ARGS
