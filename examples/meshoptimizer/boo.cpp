a0645ac7a39cb7ff8b5eb7b6a8d5295b7c499999
Rahul Sheth <rahul@snap.com>
2020-10-09 19:11:44 -0400

    Fix example

--- examples/meshoptimizer/boo.cpp
+++ examples/meshoptimizer/boo.cpp
@@ -1,8 +1,6 @@
 #include <meshoptimizer/meshoptimizer.h>
-#include <meshoptimizer/gltfpack.h>
 
 int main() {
     meshopt_remapIndexBuffer(NULL, NULL, 0, NULL);
-    printf("gltfpack %s\n", getVersion().c_str());
     return 0;
 }
