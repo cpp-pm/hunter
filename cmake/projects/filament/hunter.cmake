8c8db3fc2cf53e16e096af454fa373680eaf35c5
Bryan Bai <zbai@snapchat.com>
2021-01-20 16:49:23 -0500

    Update 'filament' to v1.9.9-p1

--- cmake/projects/filament/hunter.cmake
+++ cmake/projects/filament/hunter.cmake
@@ -13,6 +13,17 @@ hunter_add_version(
     PACKAGE_NAME
     filament
     VERSION
+    1.9.8-p0
+    URL
+    "https://github.com/cpp-pm/filament/archive/v1.9.8-p0.tar.gz"
+    SHA1
+    7ef097692ea5cd68ba34dc48c73fff96b4f98b54
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    filament
+    VERSION
     1.9.8-p1
     URL
     "https://github.com/cpp-pm/filament/archive/v1.9.8-p1.tar.gz"
@@ -24,11 +35,22 @@ hunter_add_version(
     PACKAGE_NAME
     filament
     VERSION
-    1.9.8-p0
+    1.9.9-p0
     URL
-    "https://github.com/cpp-pm/filament/archive/v1.9.8-p0.tar.gz"
+    "https://github.com/cpp-pm/filament/archive/v1.9.9-p0.tar.gz"
     SHA1
-    7ef097692ea5cd68ba34dc48c73fff96b4f98b54
+    796e87a58713cf3bd517a66a2a7722bf006f52ac
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    filament
+    VERSION
+    1.9.9-p1
+    URL
+    "https://github.com/cpp-pm/filament/archive/v1.9.9-p1.tar.gz"
+    SHA1
+    5cae823054ff29acfbdcf89cbb87239296445c81
 )
 
 hunter_cmake_args(
