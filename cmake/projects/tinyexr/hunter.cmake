669a16471c229b89ce4f7a10a52e0affc8da0b15
Rahul Sheth <rahul@snap.com>
2021-07-02 16:07:55 -0400

    Update tinyexr to v1.0.0-3c33352-p1

--- cmake/projects/tinyexr/hunter.cmake
+++ cmake/projects/tinyexr/hunter.cmake
@@ -36,9 +36,20 @@ hunter_add_version(
     VERSION
     1.0.0-3c33352-p0
     URL
-    "https://github.com/cpp-pm/tinyexr/archive/refs/tags/v1.0.0-3c33352-p0.tar.gz"
+    "https://github.com/cpp-pm/tinyexr/archive/d9ca811d2c3b9b9aaf6752b880aa43dd59f0350e.tar.gz"
     SHA1
-    9d06c3179a8e8f119c78c43eee21cb806820aea7
+    e0c64e31006de21da4464f8c062fac6048a5aa2a
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    tinyexr
+    VERSION
+    1.0.0-3c33352-p1
+    URL
+    "https://github.com/cpp-pm/tinyexr/archive/refs/tags/v1.0.0-3c33352-p1.tar.gz"
+    SHA1
+    798e6ef3697b1ecc158bd43dbf66e9c93a0a945e
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
