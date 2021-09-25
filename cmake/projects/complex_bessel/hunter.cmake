a2d879edcdd376048cb4c5a7bcca02147009f119
craffael <raffael@casagrande.ch>
2021-09-25 21:29:22 +0200

    use patched version

--- cmake/projects/complex_bessel/hunter.cmake
+++ cmake/projects/complex_bessel/hunter.cmake
@@ -12,11 +12,11 @@ hunter_add_version(
     PACKAGE_NAME
     complex_bessel
     VERSION
-    "0.5.0-4199061"
+   "0.5.0-abab4b5"
     URL
-    "https://github.com/joeydumont/complex_bessel/archive/4199061675fd2e8784daca5d85d6454571b86448.tar.gz"
+   "https://github.com/joeydumont/complex_bessel/archive/d08c44541a44c5a02490c698478bb5a8ec335861.tar.gz"
     SHA1
-    9fe22d021a183b5abc8f84d42c255dc54537dc72
+    58fedaab36b89c499f9bc73afaa5a36f16ef433c
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
