f0d4346e139dd29f38b886d8891edc5e786929aa
craffael <raffael@casagrande.ch>
2021-10-11 17:39:07 +0200

    add package ittnotify

---
+++ cmake/projects/ittapi/hunter.cmake
@@ -0,0 +1,24 @@
+# Copyright (c) 2016-2020, Rahul Sheth, Ruslan Baratov
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    ittapi
+    VERSION
+    05ac701
+    URL
+    "https://github.com/craffael/ittapi/archive/05ac701ea1277565d9b1f291ef29a09d4bb94a38.tar.gz"
+    SHA1
+    05ac701ea1277565d9b1f291ef29a09d4bb94a38
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(ittapi)
+hunter_download(PACKAGE_NAME ittapi)
---
+++ examples/ittapi/boo.cpp
@@ -0,0 +1,4 @@
+#include <ittnotify.h>
+
+int main() {
+}
