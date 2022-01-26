212d66a65a7e2d774db17fdf40cb59636c3721ab
drodin <rodin.dmitry@gmail.com>
2022-01-26 10:28:26 +0700

    check-urls.py maintenance script

---
+++ maintenance/check-urls.py
@@ -0,0 +1,49 @@
+from os.path import dirname, abspath, join
+from glob import glob
+import re
+import pycurl
+
+def getResponseStausCode(url):
+    try:
+        c = pycurl.Curl()
+        c.setopt(pycurl.WRITEFUNCTION, lambda x: None)
+        # c.setopt(pycurl.HEADERFUNCTION, lambda x: None)
+        c.setopt(pycurl.URL, url)
+        c.setopt(pycurl.HEADER, 1)
+        c.setopt(pycurl.NOBODY, 1)
+        c.setopt(pycurl.FOLLOWLOCATION, 1)
+        c.perform()
+        return c.getinfo(pycurl.HTTP_CODE)
+    except pycurl.error:
+        return 999
+
+hunterDir = dirname(dirname(abspath(__file__)))
+projectsDir = join(hunterDir, 'cmake', 'projects')
+
+projectsFiles = join(projectsDir, '**', '*.cmake')
+
+checkedFile = join(hunterDir, 'checked.txt')
+try:
+    checkedStream = open(checkedFile, "r+")
+    checked = checkedStream.readlines()
+except FileNotFoundError:
+    checkedStream = open(checkedFile, "w")
+    checked = []
+
+projects = dict()
+
+for projectFile in glob(projectsFiles, recursive=True):
+    file = open(projectFile, "r")
+    content = file.read()
+    file.close()
+
+    entries = re.findall(r'hunter_add_version\s*\(\s*PACKAGE_NAME\s+"*(.*?)"*\s+VERSION\s+"*(.*?)"*\s+URL\s+"*(.*?)"*\s+SHA1\s+"*(.*?)"*\s+.*?\)', content, re.MULTILINE | re.DOTALL)
+    if len(entries):
+        for name, version, url, _ in entries:
+            if not any(url == x.rstrip('\n') for x in checked):
+                statusCode = getResponseStausCode(url)
+                print(str(statusCode) + ' ' + url)
+                if  statusCode > 200:
+                    checkedStream.write(str(statusCode) + ' ' + url + '\n')
+                
+checkedStream.close()
