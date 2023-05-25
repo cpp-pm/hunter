b5f519b164ed0f982cdf7de4448401e6ff56724d
Shubham Avasthi <shubhamavasthi1@gmail.com>
2023-05-25 23:43:53 +0530

    Update leveldb to v1.22-p1

--- cmake/projects/leveldb/hunter.cmake
+++ cmake/projects/leveldb/hunter.cmake
@@ -15,8 +15,8 @@ hunter_add_version(
 hunter_add_version(
     PACKAGE_NAME leveldb
     VERSION "1.22"
-    URL "https://github.com/hunter-packages/leveldb/archive/v1.22-p0.tar.gz"
-    SHA1 "616e3816c0f44423136dcf286ae51417f0d75198")
+    URL "https://github.com/cpp-pm/leveldb/archive/refs/tags/v1.22-p1.tar.gz"
+    SHA1 "2b9024071ce4b43a32eca81811c6589a8530d04f")
 
 hunter_cmake_args(leveldb CMAKE_ARGS
     LEVELDB_BUILD_TESTS=OFF
