bdd8fd5bd6f36ab5fca050f373521f087b3508ae
Reinhold Gschweicher <pyro4hell@gmail.com>
2025-04-29 16:53:57 +0200

    set_matrix: add --projects override for local debugging

--- .github/workflows/set_matrix.py
+++ .github/workflows/set_matrix.py
@@ -1,7 +1,12 @@
-import os
-import sys
+#!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+
+import argparse
 import json
+import os
+import pathlib
 import re
+import sys
 
 
 # simple helper to allow single-line-comments with `//` in json files
@@ -18,61 +23,89 @@ def json_from_file_ignore_comments(filePath):
     return json_data
 
 
-try:
-    with open(os.environ.get("HOME") + "/files.json") as json_files:
-        files = json.load(json_files)
-except IOError:
-    sys.exit("Can't read changed files from files.json")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--projects",
+        help="space separated list of projects to process, used for local debugging",
+        type=str,
+    )
+    args = parser.parse_args()
 
-projects = set()
-run_hunter_tests: bool = False
+    repo_root = pathlib.Path(__file__).parent.parent.parent
+    projects_dir = repo_root / "cmake" / "projects"
+    if not projects_dir.is_dir():
+        raise RuntimeError(f"missing projects dir expected to be at {projects_dir}")
 
-p = re.compile("cmake/projects/([^/]+)")
-for file in files:
-    if p.match(file):
-        project = p.match(file).group(1)
-        if os.path.isdir("cmake/projects/" + project):
-            projects.add(project)
-    if file.startswith("cmake/modules/"):
-        run_hunter_tests = True
-    if file.startswith("cmake/schemes/"):
-        run_hunter_tests = True
-    if file.startswith("cmake/templates/"):
-        run_hunter_tests = True
-    if file.startswith("tests/"):
-        run_hunter_tests = True
-
-if projects or run_hunter_tests:
-    dafault_dir = ".github/workflows/ci/"
-
-    default_matrix = json_from_file_ignore_comments(dafault_dir + "matrix.json")
-
-    include = []
-    for project in projects:
-        project_dir = "cmake/projects/" + project + "/ci/"
-
-        matrix_override = project_dir + "matrix.json"
-        if os.path.isfile(matrix_override):
-            project_matrix = json_from_file_ignore_comments(matrix_override)
-        else:
-            project_matrix = [dict(leg, example=project) for leg in default_matrix]
-
-        for leg in project_matrix:
-            if os.path.isfile(project_dir + leg["script"]):
-                leg["script"] = project_dir + leg["script"]
+    projects = set()
+    run_hunter_tests: bool = False
+    if args.projects:
+        for project in set(args.projects.split(" ")):
+            if project == "hunter_tests":
+                run_hunter_tests = True
             else:
+                if (projects_dir / project).is_dir():
+                    projects.add(project)
+                else:
+                    raise RuntimeError(f"provided project doesn't exist: {project}")
+    else:
+        try:
+            with open(os.environ.get("HOME") + "/files.json") as json_files:
+                files = json.load(json_files)
+        except IOError:
+            raise RuntimeError("Can't read changed files from files.json")
+
+        p = re.compile("cmake/projects/([^/]+)")
+        for file in files:
+            if p.match(file):
+                project = p.match(file).group(1)
+                if os.path.isdir("cmake/projects/" + project):
+                    projects.add(project)
+            if file.startswith("cmake/modules/"):
+                run_hunter_tests = True
+            if file.startswith("cmake/schemes/"):
+                run_hunter_tests = True
+            if file.startswith("cmake/templates/"):
+                run_hunter_tests = True
+            if file.startswith("tests/"):
+                run_hunter_tests = True
+
+    if projects or run_hunter_tests:
+        dafault_dir = ".github/workflows/ci/"
+
+        default_matrix = json_from_file_ignore_comments(dafault_dir + "matrix.json")
+
+        include = []
+        for project in projects:
+            project_dir = "cmake/projects/" + project + "/ci/"
+
+            matrix_override = project_dir + "matrix.json"
+            if os.path.isfile(matrix_override):
+                project_matrix = json_from_file_ignore_comments(matrix_override)
+            else:
+                project_matrix = [dict(leg, example=project) for leg in default_matrix]
+
+            for leg in project_matrix:
+                if os.path.isfile(project_dir + leg["script"]):
+                    leg["script"] = project_dir + leg["script"]
+                else:
+                    leg["script"] = dafault_dir + leg["script"]
+
+            include += project_matrix
+
+        if run_hunter_tests:
+            hunter_tests_matrix = json_from_file_ignore_comments(
+                dafault_dir + "matrix_hunter_tests.json"
+            )
+            for leg in hunter_tests_matrix:
                 leg["script"] = dafault_dir + leg["script"]
+            include += hunter_tests_matrix
 
-        include += project_matrix
+        print(json.dumps({"include": include}))
+    else:
+        print("No projects found")
+        return 1
 
-    if run_hunter_tests:
-        hunter_tests_matrix = json_from_file_ignore_comments(
-            dafault_dir + "matrix_hunter_tests.json"
-        )
-        for leg in hunter_tests_matrix:
-            leg["script"] = dafault_dir + leg["script"]
-        include += hunter_tests_matrix
 
-    print(json.dumps({"include": include}))
-else:
-    sys.exit("No projects found")
+if __name__ == "__main__":
+    sys.exit(main())
