e85ec92a6e5a233c4e03eb9b11f2c47541277648
Michael Tesch <tesch1@gmail.com>
2026-08-21 14:47:37 -0500

    cppduals: add version 0.9.2 (#860)
    use version 0.9.2
    
    0.9.1 does not configure under a toolchain that sets CMAKE_CXX_STANDARD
    as a normal variable; 0.9.2 fixes that.

---
+++ cmake/projects/cppduals/hunter.cmake
@@ -0,0 +1,32 @@
+# Copyright (c) 2026, Michael Tesch
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    cppduals
+    VERSION
+    0.9.2
+    URL
+    "https://gitlab.com/tesch1/cppduals/-/archive/v0.9.2/cppduals-v0.9.2.tar.gz"
+    SHA1
+    1478c1fe45269470182f0222056c27ab0e22dda8
+)
+
+hunter_cmake_args(
+    cppduals
+    CMAKE_ARGS
+        CPPDUALS_TESTING=OFF
+        CPPDUALS_BENCHMARK=OFF
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(cppduals)
+hunter_download(PACKAGE_NAME cppduals)
---
+++ docs/packages/pkg/cppduals.rst
@@ -0,0 +1,27 @@
+.. spelling:word-list::
+
+    cppduals
+    autodiff
+
+.. index:: math ; cppduals
+
+.. _pkg.cppduals:
+
+cppduals
+========
+
+-  https://gitlab.com/tesch1/cppduals
+-  `Documentation <https://tesch1.gitlab.io/cppduals/>`__
+-  Maintainer: https://github.com/tesch1
+
+Header-only dual number library for exact forward-mode automatic
+differentiation, with optional Eigen integration.
+
+If you use this in research, please cite
+`doi:10.21105/joss.01487 <https://doi.org/10.21105/joss.01487>`__
+(Tesch, JOSS 4(43):1487, 2019).
+
+.. literalinclude:: /../examples/cppduals/CMakeLists.txt
+  :language: cmake
+  :start-after: # DOCUMENTATION_START {
+  :end-before: # DOCUMENTATION_END }
